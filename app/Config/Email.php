c0fe03fdc11fc4e120c5d29117f0f4c4eac8237a
davidanthonyn <55152980+davidanthonyn@users.noreply.github.com>
2022-10-02 21:48:01 +0700

    login and register
    Sistem bisa melakukan login dan register sebagai kasir, karena $defaultUserGroup pada Auth.php di myth/auth library adalah kasir.

--- app/Config/Email.php
+++ app/Config/Email.php
@@ -9,12 +9,12 @@ class Email extends BaseConfig
     /**
      * @var string
      */
-    public $fromEmail;
+    public $fromEmail = 'apotekxyz@gmail.com';
 
     /**
      * @var string
      */
-    public $fromName;
+    public $fromName = 'Apotek99Bogor';
 
     /**
      * @var string
